15cc76aee96b7ab5f8b47d67c2b5a37634cd04be
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-29 18:25:11 +0200

    fix: update model.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,43 @@
 from pydantic import BaseModel
 from datetime import datetime
+from enum import Enum
+from typing import List
+
+class LogLevel(str, Enum):
+    fatal = "fatal"
+    error = "error"
+    warn = "warn"
+    info = "info"
+    debug = "debug"
+    empty = ""
+
+class LogService(str, Enum):
+    auth = "auth"
+    payment = "payment"
+    notifications = "notifications"
+    empty = ""
 
 class LogEntry(BaseModel):
-    timestamp: datetime
-    level: str
+    level: LogLevel
     message: str
+    service: LogService
+
+class LogEntryResponse(LogEntry):
+    id: str
+    message: str
+    level: str
     service: str
+    timestamp: str
+
+class SearchLogResult(BaseModel):
+    id: str
+    message: str
+    level: str
+    service: str
+    timestamp: str
+
+class SearchLogResponse(BaseModel):
+    total: int
+    page: int
+    size: int
+    results: List[SearchLogResult]
