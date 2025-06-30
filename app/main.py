335db1a9ad0d5a222b696be36ddb456f2e531be3
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-30 22:00:53 +0200

    fix: change origins in main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["http://localhost:5173"]
+origins = ["http://localhost:4173"]
 
 app.add_middleware(
     CORSMiddleware,
