f575af7bb2092e3888a41845a374c3f1454b7b58
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-30 22:28:02 +0200

    fix: udpate Main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ origins = ["http://localhost:4173"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
